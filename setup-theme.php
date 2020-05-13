95f9fb3c7e330bda6480e8ea7c82e15231af0c93
6clicks <john@6clicks.ch>
2020-05-13 08:45:16 +0200

    mise à jour du logo

--- setup-theme.php
+++ setup-theme.php
@@ -1,29 +1,29 @@
 <?php
 /**
- * Plugin Name: Tutorials Installation du theme Snow.
- * Plugin URI: https://www.925.ch
+ * Plugin Name: Tutorials Iutilisation WP.
+ * Plugin URI: https://www.6clicks.ch
  * Description:  La liste des vidéos tutotial pour la gestion du site
  * Version:  1.0
  * Author: John Robert-Nicoud
- * Author URI: https://www.925.ch
+ * Author URI: https://www.clicks.ch
  * License: A "Slug" license name e.g. GPL2
  */
  
- add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+ add_action('wp_dashboard_setup', 'my_video_dashboard_widgets');
 
-function my_custom_dashboard_widgets() {
+function my_video_dashboard_widgets() {
 global $wp_meta_boxes;
 
-wp_add_dashboard_widget('custom_help_widget', '!! Ne pas oublier !!', 'custom_dashboard_help');
+wp_add_dashboard_widget('video_help_widget', 'Tutorials Vidéos', 'video_dashboard_help');
 }
 
-function custom_dashboard_help() {
+function video_dashboard_help() {
 echo '
 <img src="https://raw.githubusercontent.com/6clicks/tuto-snow/master/logo-925.png">
-<h2>tutorial pour installer SNOW </h2>
+<h2>tutorial L\'utilisation de WordPress </h2>
 <ol>
-<li> une fois installé le thème vous pouvez masquer cette fenêtre avec les option d\'écrant</li>
-<li> Importer les ACF pour les images résponsive <a href="https://raw.githubusercontent.com/6clicks/tuto-snow/master/acf-images-responsive.json" target="_blanc">télécharger ce fichier </a> et l\' importer dans ACF / outils	</li>
+<li> Si vous maitriser votre WordPress vous pouvez masquer cette fenêtre avec les option d\'écrant</li>
+<li> <a href=" " target="_blanc" >Introduction à l\admin de Wordpress</a></li>
 </ol>
 <br>
 ';
